2b5245df12aae1e61205c95a8c1145b54e6035e4
marco79811 <100792204+marco79811@users.noreply.github.com>
2023-03-15 18:52:29 +0800

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,37 +1,55 @@
-// 獲取輸入框
-var inputBox = document.getElementById("inputBox");
+// // 獲取輸入框
+// var inputBox = document.getElementById("inputBox");
 
-// 加
-function add() {
-  inputBox.value += "+";
+// // 加
+// function add() {
+//   inputBox.value += "+";
+// }
+
+// // 減
+// function subtract() {
+//   inputBox.value += "-";
+// }
+
+// // 乘
+// function multiply() {
+//   inputBox.value += "×";
+// }
+
+// // 除
+// function divide() {
+//   inputBox.value += "/";
+// }
+
+// // 清除輸入框
+// function clearInput() {
+//   inputBox.value = "";
+// }
+
+// // 計算
+// function calculate() {
+//   try {
+//     var result = eval(inputBox.value);
+//     inputBox.value = result;
+//   } catch (error) {
+//     alert("Error");
+//   }
+// }
+
+
+let input = "";
+let result = document.getElementById("result");
+
+function addInput(value) {
+  input += value;
+  result.value = input;
 }
 
-// 減
-function subtract() {
-  inputBox.value += "-";
-}
-
-// 乘
-function multiply() {
-  inputBox.value += "×";
-}
-
-// 除
-function divide() {
-  inputBox.value += "/";
-}
-
-// 清除輸入框
-function clearInput() {
-  inputBox.value = "";
-}
-
-// 計算
 function calculate() {
-  try {
-    var result = eval(inputBox.value);
-    inputBox.value = result;
-  } catch (error) {
-    alert("Error");
-  }
+  result.value = eval(input);
+  input = "";
 }
+
+function clearResult() {
+ 
+
